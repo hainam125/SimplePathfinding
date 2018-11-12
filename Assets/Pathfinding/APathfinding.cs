c42f3bcf4ebe2a267f52dcb43676ca377ad023da
GNT-GLOBAL\nam.nh2388 <nam.nh2388@gnt.com.vn>
2018-11-12 14:08:22 +0700

    Update scene

--- Assets/Pathfinding/APathfinding.cs
+++ Assets/Pathfinding/APathfinding.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class APathfinding {
 	private static Grid grid;
--- Assets/Pathfinding/Node.cs
+++ Assets/Pathfinding/Node.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 
 public class Node {
     public static int currentId;
